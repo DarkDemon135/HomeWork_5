0d4f6aef213b76a2299621c6d3b5e4b1458eaae6
DarkDemon135 <122680581+DarkDemon135@users.noreply.github.com>
2023-02-16 18:06:31 +0000

    Main

--- H/w_09.02.23/3.cpp
+++ H/w_09.02.23/3.cpp
@@ -1,80 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-using namespace std;
-struct Listitem
+typedef struct Item* pItem;
+struct Item
 {
-    int value;
-    Listitem *next;
-    Listitem ();
+    int data;
+    pItem next;
 };
+
+void push(pItem* stackp, int data)
+{
+    pItem newp = (pItem)malloc(sizeof(struct Item));
+    newp->data = data;
+    newp->next = *stackp;
+    *stackp = newp;
+}
+
+void pop(pItem* stackp)
+{
+    pItem p = (*stackp)->next;
+    free(*stackp);
+    *stackp = p;
+}
+
+int main()
+{
+    int i;
+    pItem stackp = NULL;
  
-class Stack 
-{
-private:
-    Listitem *head;
-    Listitem *tail;
-public:
-    Stack ();
-    void push (int value);
-    void pop ();
-    int top();
-    void show ();
-    ~Stack ();
-};
+    for(i = 1; i <= 10; ++i) push(&stackp, i);
  
-Listitem::Listitem ()
-{
-    next=NULL;
-    this->value=0;
-}
-Stack::Stack()
-{
-    head=tail=NULL;
-}
-void Stack::push (int value) 
-{
-    Listitem *temp=new Listitem;
-    temp->value=value;
-    if(tail==NULL)
-        tail=temp;
-    else{
-        temp->next=tail;
-        tail=temp;
+    while(stackp)
+    {
+       printf("%d\n", stackp->data);
+       pop(&stackp);
     }
-}
-void Stack::pop () 
-{
-    if(tail==NULL){
-        cout<<"Стек пуст";
-        return;
-    }
-    Listitem *temp=tail;
-    tail=tail->next;
-    delete temp;
-}
-int Stack::top ()
-{
-    if(tail==NULL){
-        cout<<"Стек пуст";
-        return 0;
-    }
-    return tail->value;
-}
-void Stack::show () 
-{
-    if(tail==NULL){
-        cout<<"Стек пуст";
-        return;
-    }
-    Listitem *temp=tail;
-    while (temp!=NULL){
-        cout<<temp->value<<" ";
-        temp=temp->next;
-    }
-}
-Stack::~Stack ()
-{
-    delete head;
-    delete tail;
+ 
+    return 0;
 }
